44e700064819247363764897456a1ee9568338b5
JMoloughney <github email address>
2017-11-25 11:29:23 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,72 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
+  
   @@all = []
 
   def self.all
     @@all
   end
 
+  def self.create
+  	song = self.new
+  	@@all << song
+  	song
+  end
+
+
+  def self.create_by_name(name)
+  	song = self.new
+  	song.name = name
+  	@@all << song
+  	song
+  end
+
+  def self.find_by_name(name)
+  	self.all.detect do |song|
+  		song.name == name
+  	end
+  end
+
+  def self.find_or_create_by_name(name)
+  	self.find_by_name(name) || self.create_by_name(name)
+  end
+
+
+  def self.new_by_name(name)
+  	song = self.new
+  	song.name = name
+  	song 
+end
+
   def save
     self.class.all << self
+  end
+
+  def self.alphabetical
+  	self.all.sort_by {|song| song.name}
+  end
+
+  def self.new_from_filename(file_name)
+  	song = self.new
+  	file_data = file_name.split(" - ").map {|data| data.gsub(".mp3","")}
+  	song.name = file_data[1]
+  	song.artist_name = file_data[0]
+  	song
+  end
+
+  def self.create_from_filename(file_name)
+  	song = self.new
+  	file_data = file_name.split(" - ").map {|data| data.gsub(".mp3","")}
+  	song.name = file_data[1]
+  	song.artist_name = file_data[0]
+  	@@all << song
+  	song
+  end
+
+  def self.destroy_all
+  	self.all.clear
   end
 
 end
