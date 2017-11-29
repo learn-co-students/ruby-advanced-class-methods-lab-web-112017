c1c2b7dabc621c48db00a516d1df79fea5a442bc
JMoloughney <github email address>
2017-11-29 16:47:01 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,72 +1,61 @@
-require 'pry'
-
 class Song
-  attr_accessor :name, :artist_name
-  
+
   @@all = []
+   attr_accessor :name, :artist_name
+
+
+  def self.create
+    new_song = self.new
+    @@all << new_song
+    new_song
+  end
+
+
+  def self.new_by_name(name)
+    new_song = self.new
+    new_song.name = name
+    new_song
+  end
+
+  def self.create_by_name(name)
+    new_song = self.new_by_name(name)
+    @@all << new_song
+    new_song
+  end
+
+  def self.find_by_name(name)
+    self.all.find {|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+
+  def self.new_from_filename(file_name)
+    new_song = self.new
+    song_arr = file_name.split(" - ").map {|x| x.gsub(".mp3","")}
+    new_song.name = song_arr[1]
+    new_song.artist_name = song_arr[0]
+    new_song
+  end
+
+  def self.create_from_filename(file_name)
+    new_song = self.new_from_filename(file_name)
+    @@all << new_song 
+    new_song
+  end
+
+  def self.alphabetical
+    self.all.sort_by {|song| song.name}
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
 
   def self.all
     @@all
   end
 
-  def self.create
-  	song = self.new
-  	@@all << song
-  	song
-  end
-
-
-  def self.create_by_name(name)
-  	song = self.new
-  	song.name = name
-  	@@all << song
-  	song
-  end
-
-  def self.find_by_name(name)
-  	self.all.detect do |song|
-  		song.name == name
-  	end
-  end
-
-  def self.find_or_create_by_name(name)
-  	self.find_by_name(name) || self.create_by_name(name)
-  end
-
-
-  def self.new_by_name(name)
-  	song = self.new
-  	song.name = name
-  	song 
-end
-
-  def save
-    self.class.all << self
-  end
-
-  def self.alphabetical
-  	self.all.sort_by {|song| song.name}
-  end
-
-  def self.new_from_filename(file_name)
-  	song = self.new
-  	file_data = file_name.split(" - ").map {|data| data.gsub(".mp3","")}
-  	song.name = file_data[1]
-  	song.artist_name = file_data[0]
-  	song
-  end
-
-  def self.create_from_filename(file_name)
-  	song = self.new
-  	file_data = file_name.split(" - ").map {|data| data.gsub(".mp3","")}
-  	song.name = file_data[1]
-  	song.artist_name = file_data[0]
-  	@@all << song
-  	song
-  end
-
-  def self.destroy_all
-  	self.all.clear
-  end
 
 end
